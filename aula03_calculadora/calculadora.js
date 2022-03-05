a6b3f3210ec0787f6f991979f3804d18d465cbeb
Rafarinh4 <rafaelcamargo089@gmail.com>
2022-03-05 16:50:51 -0300

    Alteração na calculadora

--- aula03_calculadora/calculadora.js
+++ aula03_calculadora/calculadora.js
@@ -3,20 +3,20 @@ import funcaoSubtrair from './subtrair.js';
 import funcaoMultiplicar from './multiplicar.js';
 import funcaoDividir from './dividir.js';
 
-let operacao = prompt("Informe a operação:  '+', '-', '/' ou '*' ");
+let operacao = prompt("Informe a operação:  '+', '*', '/' ou '-' ");
 let valor1 = parseInt(prompt("Insira o primeiro número"));
 let valor2 = parseInt(prompt("Insira o segundo valor"));
 
 if (operacao === '-') {
-    alert(`Resultado: ${funcaoSubtrair(valor1, valor2)}`);
+    alert(`Resultado da operação: ${funcaoSubtrair(valor1, valor2)}`);
 }
 if (operacao === '+') {
-    alert(`Resultado: ${funcaoSoma(valor1, valor2)}`);
+    alert(`Resultado da operação: ${funcaoSoma(valor1, valor2)}`);
 }
 if (operacao === '/') {
-    alert(`Resultado: ${funcaoDividir(valor1, valor2)}`);
+    alert(`Resultado da operação: ${funcaoDividir(valor1, valor2)}`);
 }
 if (operacao === '*') {
-    alert(`Resultado: ${funcaoMultiplicar(valor1, valor2)}`);
+    alert(`Resultado da operação: ${funcaoMultiplicar(valor1, valor2)}`);
 }
 
